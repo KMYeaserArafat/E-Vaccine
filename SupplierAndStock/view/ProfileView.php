04c51b774af499c79804f7eee5409e5a7a451b72
Arafat <kmyeaserarafat1971@gmail.com>
2024-04-07 16:41:44 +0600

    @update_Profile_CSS

--- SupplierAndStock/view/ProfileView.php
+++ SupplierAndStock/view/ProfileView.php
@@ -10,11 +10,36 @@ include '../controller/ProfileController.php';
 <html>
     <head>
         <title>Profile</title>
+        <link rel="icon" href="../Documents/titleLoginPhoto.png"/>
+        <link rel="stylesheet" href="../Design/Profile.css">
     </head>
 
     <body>
-        <h1>Profile</h1>
-        <p><?php echo(showInformation($showResult)); ?></p>
+        <div class="headDiv">
+            <img class="logo" src="../Documents/VaccineLogo.png" alt="Vaccine Logo">
+            <p class="title">E-Vaccine</p>
+        </div>
+
+
+        <div class="DashBoard">
+           <img  class="dashBoardLogo" src="../Documents/DashBoard.png">
+           <a class="dashBoardLink" href="../view/DashBoard.php">Back to DashBoard</a>
+        </div>
+
+        <div>
+            <img class="userProfilePhoto" src="../Documents/userProfile.png">
+            <h1 id="userProfile">User Profile</h1>
+            <p id="table"><?php echo(showInformation($showResult)); ?></p>
+        </div>
+
+
+        <div class="footerDiv">
+            <p id="footerPera">Develop by : K M Yeaser Arafat</p>
+        </div>
+
+
+
+
     
     </body>
 </html>
