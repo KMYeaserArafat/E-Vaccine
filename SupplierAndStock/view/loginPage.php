15f9be3941c2032659db77beff621cfa710aa7fe
Arafat <kmyeaserarafat1971@gmail.com>
2024-04-20 16:58:24 +0600

    login Page Update

--- SupplierAndStock/view/loginPage.php
+++ SupplierAndStock/view/loginPage.php
@@ -14,44 +14,42 @@ include '../controller/LoginController.php';
     </head>
 
     <body>
-        <div id="div1">
-        <img id="logo" src="../Documents/VaccineLogo.png" alt="vaccine Logo" height="64px" width="64px">
-        <h1 id="headTitle">E-Vaccine</h1>
-        </div>
-        <form method="POST" action=""  onsubmit="return validateForm()" >
-           <!-- <fieldset> -->
-            <div id="formPanel">
-            <img id="loginLogo" src="../Documents/loginLogo.png" alt="Login Logo">
-            <legend>Login</legend>
-            <p id="message"><?php echo($message) ?>
-            </p>
-            <table>
-                <tr>
-                    <td class="label">Email : </td>
-                    <td><input type="text" name="email" id="email"></td>
-                </tr>
-
-                <tr>
-                    <td class="label">Password : </td>
-                    <td class="inputBox"><input type="password" name="password" id="password"></td>
-                </tr>
-
-                <tr>
-                    <td></td>
-                    <td id="cheackBox"><input type="checkbox" name="cheackBox">Show Password</td>
-                </tr>
-
-                <tr>
-                    <td></td>
-                    <td><input class="btnSubmit" type="submit" name="submit" value="Login">
-                        <input class="btnReset" type="submit" name="reset" value="Reset">
-                </td>
-                </tr>
-            </table>
-           <!-- </fieldset> -->
+        <div class="background">
+            <div id="div1">
+                <img id="logo" src="../Documents/VaccineLogo.png" alt="vaccine Logo" height="64px" width="64px">
+                <h1 id="headTitle">E-Vaccine</h1>
             </div>
 
-        </form>
+            <div class="loginDiv">
+                <img id="loginLogo" src="../Documents/loginLogo.png" alt="Login Logo">
+                <h2 id="loginHeadTitle">LOGIN</h2>
+                <p id="message"><?php echo($message) ?>
+                <form method="POST" action=""  onsubmit="return validateForm()" >
+                <table>
+                    <tr>
+                        <td class="label">Email : </td>
+                        <td><input type="text" name="email" id="email"></td>
+                    </tr>
+
+                    <tr>
+                        <td class="label">Password : </td>
+                        <td class="inputBox"><input type="password" name="password" id="password"></td>
+                    </tr>
+
+
+                    <tr>
+                        <td></td>
+                        <td><input class="btnSubmit" type="submit" name="submit" value="Login">
+                        <input class="btnReset" type="submit" name="reset" value="Reset"></td>
+                    </tr>
+                </table>
+                </form>
+                
+                <a href="./RegistrationSupplier.php" id="registrationLink">If you haven't any Account, Register here!!</a>
+            </div>
+        </div>
+
+
         <script src="../js/loginScript.js"></script>
     </body>
 </html>
